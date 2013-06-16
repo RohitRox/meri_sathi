4bb0ddc56a5631d29dca312944a35cf21c0bb79a
Rohitrox <link2j.roy@gmail.com>
2013-06-16 09:19:25 +0545

    rails initial with mongoid

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MeriSathi::Application.config.secret_token = '645b4e11943a7274e163fb526c57f7d0d5ac2eac89b4f73e4c1037260101376285be528e397f160c5f6ffc059074ddc6ba394eb399ce6f37af0336af5f86f076'
